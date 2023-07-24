91e5255eb347cb5c8516d92afeb449d1cca462cb
estrella <epopo2@illinois.edu>
2023-07-23 22:12:56 -0500

    kinda working code

--- app/layout.js
+++ app/layout.js
@@ -12,6 +12,9 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+      <link href="https://fonts.googleapis.com/css2?family=Roboto+Mono:ital,wght@0,400;0,500;0,600;1,400;1,500;1,600&display=swap" rel="stylesheet"></link>
     </html>
   )
 }
